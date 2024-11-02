410990f58ba7d515ec96e3e216ad7df338148f2b
Karl Enberg <karl.enberg@britepayments.com>
2024-11-02 09:19:40 +0100

    Initial commit, v1.0

--- vertexai/app/build.gradle.kts
+++ vertexai/app/build.gradle.kts
@@ -22,11 +22,11 @@ plugins {
 }
 
 android {
-    namespace = "com.google.firebase.quickstart.vertexai"
+    namespace = "com.formulae.chef"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.google.firebase.quickstart.vertexai"
+        applicationId = "com.formulae.chef"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
@@ -56,6 +56,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.preference:preference-ktx:1.1.1")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     implementation(platform("androidx.compose:compose-bom:2024.10.00"))
     implementation("androidx.compose.ui:ui")
---
+++ vertexai/app/src/main/kotlin/com/formulae/chef/GenerativeAiViewModelFactory.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.formulae.chef
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.google.firebase.Firebase
+import com.formulae.chef.feature.chat.ChatViewModel
+import com.google.firebase.vertexai.type.content
+import com.google.firebase.vertexai.type.generationConfig
+import com.google.firebase.vertexai.vertexAI
+
+val GenerativeViewModelFactory = object : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(
+        viewModelClass: Class<T>,
+        extras: CreationExtras
+    ): T {
+        val config = generationConfig {
+            temperature = 1.5f
+            maxOutputTokens = 8192
+            topP = 0.95f
+        }
+
+        return with(viewModelClass) {
+            when {
+                isAssignableFrom(ChatViewModel::class.java) -> {
+                    // Initialize a GenerativeModel with the `gemini-flash` AI model for chat
+                    val generativeModel = Firebase.vertexAI.generativeModel(
+                        modelName = "gemini-1.5-flash-002",
+                        generationConfig = config,
+                        systemInstruction = content { text("You are a personal chef / cooking assistant to help with coming up for new ideas on recipes. Use https://www.honestgreens.com/en/menu as inspiration for the whole foods, healthy, simple and savory cooking / recipe style. Please use metric units and centiliters / decilitres for liquid measurements and state the nutritional values for each recipe.") }
+                    )
+                    ChatViewModel(
+                        context = extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as Context,
+                        generativeModel = generativeModel
+                    )
+                }
+
+                else ->
+                    throw IllegalArgumentException("Unknown ViewModel class: ${viewModelClass.name}")
+            }
+        } as T
+    }
+}
